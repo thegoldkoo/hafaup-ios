a06e499793b5f0cffc7cd9577006800a98c0dc0c
thegoldkoo <innovation@captainguam.com>
2026-04-19 14:27:20 +1000

    Update Settings.swift

--- HafaUp/Settings.swift
+++ HafaUp/Settings.swift
@@ -1,29 +1,27 @@
 import WebKit
-
 struct Cookie {
     var name: String
     var value: String
 }
-
-let gcmMessageIDKey = "00000000000" // update this with actual ID if using Firebase 
-
+let gcmMessageIDKey = "00000000000"
 // URL for first launch
 let rootUrl = URL(string: "https://hafaup.com/app.html")!
-
 // allowed origin is for what we are sticking to pwa domain
 // This should also appear in Info.plist
 let allowedOrigins: [String] = ["hafaup.com"]
-
 // auth origins will open in modal and show toolbar for back into the main origin.
 // These should also appear in Info.plist
-let authOrigins: [String] = []
+let authOrigins: [String] = [
+    "ap-southeast-2m8nfu05hd.auth.ap-southeast-2.amazoncognito.com",
+    "accounts.google.com",
+    "kauth.kakao.com",
+    "appleid.apple.com"
+]
 // allowedOrigins + authOrigins <= 10
-
 let platformCookie = Cookie(name: "app-platform", value: "iOS App Store")
-
 // UI options
-let displayMode = "standalone" // standalone / fullscreen.
-let adaptiveUIStyle = true     // iOS 15+ only. Change app theme on the fly to dark/light related to WebView background color.
-let overrideStatusBar = false   // iOS 13-14 only. if you don't support dark/light system theme.
-let statusBarTheme = "dark"    // dark / light, related to override option.
-let pullToRefresh = true    // Enable/disable pull down to refresh page
+let displayMode = "standalone"
+let adaptiveUIStyle = true
+let overrideStatusBar = false
+let statusBarTheme = "dark"
+let pullToRefresh = true
